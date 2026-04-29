95d6b142a680427e36aff8453c8fd7b1a39dffba
Moustafa Fouda <foudamoustafa132@gmail.com>
2026-04-30 00:14:53 +0300

    وصف التعديل

--- lib/ids.dart
+++ lib/ids.dart
@@ -1,15 +1,11 @@
-List<Map<String,dynamic>>ids=[
-
-{"title":"the winner taks it","id":"watch?v=WbnG3eAGb6Y&list=RDXj0-73cK3ow&index=2"} , 
-
-{"title":"بهاء سلطان - معايا (أغنية فيلم ولنا فى الخيال ... حب؟","id":"watch?v=HVgy8Ge0GJQ&list=RDXj0-73cK3ow&index=15"} , 
-
-{"title":" بيلي جين","id":"watch?v=Kr4EQDVETuA&list=RDXj0-73cK3ow&index=3"} , 
-{"title":" فلكسين","id":"watch?v=Xxg2ZmTEldg&list=RDXj0-73cK3ow&index=5"} , 
-{"title":" حسيني","id":"watch?v=bcMIr1xhXQI&list=RDXj0-73cK3ow&index=6"} , 
-{"title":"غريب حالي ","id":"watch?v=xvDyGpEEcfM&list=RDXj0-73cK3ow&index=8"} , 
-{"title":"حبيبي دا ","id":"watch?v=gl3wJd1bldQ&list=RDXj0-73cK3ow&index=9"} , 
-{"title":" باليني","id":"watch?v=cjJpU1OhbTk&list=RDXj0-73cK3ow&index=14"} , 
-{"title":"علي باب السيما ","id":"watch?v=EWcvSxIqMfA&list=RDXj0-73cK3ow&index=22"} , 
-
+List<Map<String,dynamic>> ids = [
+  {"title":"the winner takes it all","id":"WbnG3eAGb6Y"},
+  {"title":"بهاء سلطان - معايا","id":"HVgy8Ge0GJQ"},
+  {"title":"بيلي جين","id":"Kr4EQDVETuA"},
+  {"title":"فلكسين","id":"Xxg2ZmTEldg"},
+  {"title":"حسيني","id":"bcMIr1xhXQI"},
+  {"title":"غريب حالي","id":"xvDyGpEEcfM"},
+  {"title":"حبيبي دا","id":"gl3wJd1bldQ"},
+  {"title":"باليني","id":"cjJpU1OhbTk"},
+  {"title":"علي باب السيما","id":"EWcvSxIqMfA"},
 ];
